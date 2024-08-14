7b308c9a47f064c08ae85e042874dd005497c15d
Roger <128326832+RogerRos@users.noreply.github.com>
2024-08-14 23:30:46 +0200

    Update openmain.bat

--- openmain.bat
+++ openmain.bat
@@ -1,3 +1,5 @@
 start "" pythonw "C:\Users\...\main.py" 
 
 # The address to the main.py file (This is intended to be a comment but I don't know how to put them in .bat... Windows stuff.)
+
+# [I highly recommend reading the readme]
