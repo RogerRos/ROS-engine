b8748d3489e1a0b0e962b0264ccbb6bfb0a9994e
Roger <128326832+RogerRos@users.noreply.github.com>
2024-08-15 02:41:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,33 +15,37 @@ def on_key_press(event):
 # GUI Settings
 def create_gui():
     root = tk.Tk()
-    root.title("ROSengine")
+    root.title("ROS ENGINE")
 
-    # Create a frame for the images
+    # Create a frame for the images and numbers.
     frame = tk.Frame(root)
-    frame.grid(row=0, column=0, columnspan=4, pady=10)  # Ajustado a 4 columnas
+    frame.grid(row=0, column=0, pady=10)
 
-    # Iages.
+    # Upload images and labels to display them
     images = []
-    for i in range(1, 2):  # The numbers and images must be the same. This is ready to have 1 action at the moment.
+    for i in range(1, 2): # The numbers and images must be the same. This is ready to have 1 action at the moment.
         image = Image.open(f"image_{i}.png")
-        # Redimensionar la imagen manteniendo la relación de aspecto
         image.thumbnail((100, 100))
         photo = ImageTk.PhotoImage(image)
-        label = tk.Label(frame, image=photo)
-        label.grid(row=0, column=i-1, padx=5)
+        
+        # Create a sub-frame for each image and its corresponding number
+        sub_frame = tk.Frame(frame)
+        sub_frame.grid(row=0, column=i-1, padx=5)
+
+        # Add the image to the sub-frame
+        label_image = tk.Label(sub_frame, image=photo)
+        label_image.grid(row=0, column=0)
         images.append(photo)
 
-    # Numbers.
-    for i in range(1, 2): # The numbers and images must be the same. This is ready to have 1 action at the moment.
-        label = tk.Label(root, text=str(i))
-        label.grid(row=1, column=i-1, pady=5)
-
-  # Note: It is recommended that each function that is put like (open_gmail, open_folder...) should have its image in the same folder as the main.py script.
+        # Add the number below the image in the sub-frame
+        label_number = tk.Label(sub_frame, text=str(i))
+        label_number.grid(row=1, column=0)
 
     root.bind('<Key>', on_key_press)
 
     root.mainloop()
+
+ # Note: It is recommended that each function that is put like (open_gmail, open_folder...) should have its image in the same folder as the main.py script.
 
 def open_gmail():
     chrome_path = r"" # The address of chrome.exe on your computer.
